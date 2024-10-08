ccab1b321562c1ba60b49e69ce725f5f249d185a
eunicewu4 <eunicew6@nycstudents.net>
2024-10-08 17:55:58 -0400

    lec 5

--- Wu_Eunice_Lecture_5.cpp
+++ Wu_Eunice_Lecture_5.cpp
@@ -1,5 +1,6 @@
-// 
-
+// Eunice Wu
+// October 8, 2024
+// Lecture 5
 
 #include <iostream>
 #include <string>
@@ -7,37 +8,44 @@
 using namespace std;
 
 int main ()
-
 {
 
 double bal;
 double trans;
-
-cout << "What is your balance and how many transitions did you make today? (balance transactions) ";
-cin >> bal >> trans;
-
 string type;
 double amt;
 double credtot;
 double debtot;
 
+cout << "What is your balance and how many transitions did you make today? (balance transactions) "; // ask and stores 
+cin >> bal >> trans;
+
+//loop that sums up all the transactions to find the total balance of the account, debit, and credit
 while (trans>0) {
     cout << "Is the transcation debit or credit and how much (debit/credit amount)? ";
     cin >> type >> amt;
 
     if (type == "credit")
-    {bal = bal + amt;
-    credtot= credtot + amt;}
+        {bal = bal + amt;
+        credtot= credtot + amt;} // sums up the credit and debit total amount
     else
-    {bal = bal - amt;
-    debtot= debtot + amt;}
-
+        {bal = bal - amt;
+        debtot= debtot + amt;}
+    
     trans=trans-1;
 }
 
-cout << "Your ending balance is: $" << bal << fixed << setprecision (2) << "\n";
-cout << "Debit total: -$" << debtot << " Credit total: $" << credtot;
+cout << "Your ending balance is: $" << bal << fixed << setprecision (2) << "\n"; // prints ending balance after asking about all the transaction. added $ notation and set to 2 decimal places
+cout << "Debit total: -$" << debtot << " Credit total: $" << credtot; // prints credit and debit total
 
 
     return 0;
 }
+/*
+What is your balance and how many transitions did you make today? (balance transactions) 30 3
+Is the transcation debit or credit and how much (debit/credit amount)? credit 100
+Is the transcation debit or credit and how much (debit/credit amount)? debit 1
+Is the transcation debit or credit and how much (debit/credit amount)? credit 4
+Your ending balance is: $133
+Debit total: -$1.00 Credit total: $104.00
+*/
